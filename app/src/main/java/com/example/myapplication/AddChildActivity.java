df0d575413ca4d5a28d35005d273ef570f8f765c
Ggamid <khalidov-gamid@mail.ru>
2022-06-09 17:13:17 +0300

    добавлены данные для ребенка такие как возраст, смесь, вес в AddChildActivity добавлена проверка заполнены ли все поля или нет

--- app/src/main/java/com/example/myapplication/AddChildActivity.java
+++ app/src/main/java/com/example/myapplication/AddChildActivity.java
@@ -6,12 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class AddChildActivity extends AppCompatActivity {
-    EditText firstName,lastName,fatherName, birthDay, age;
+    EditText firstName,lastName,fatherName, birthDay, age, weight;
+    Spinner smes;
     DatabaseReference firebaseDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,17 +23,26 @@ public class AddChildActivity extends AppCompatActivity {
         init();
     }
     public void init(){
-        firstName = findViewById(R.id.editTextTextPersonName3);
-        lastName = findViewById(R.id.editTextTextPersonName4);
-        fatherName = findViewById(R.id.editTextTextPersonName5);
+        firstName = findViewById(R.id.childName);
+        lastName = findViewById(R.id.childLastName);
+        fatherName = findViewById(R.id.childFatherName);
         birthDay = findViewById(R.id.RegActivityBirthDay);
         age = findViewById(R.id.Age);
+        weight = findViewById(R.id.weight);
+        smes = findViewById(R.id.spinner);
         firebaseDatabase  = FirebaseDatabase.getInstance().getReference("Child");
+
+
     }
     public void setBD(View view){
-
-        Child child = new Child(firstName.getText().toString(),lastName.getText().toString(),fatherName.getText().toString(), birthDay.getText().toString());
-        firebaseDatabase.push().setValue(child);
-        startActivity(new Intent(AddChildActivity.this, MainActivity.class));
+        if(!firstName.getText().toString().equals("") && !lastName.getText().toString().equals("") && !fatherName.getText().toString().equals("") && !age.getText().toString().equals("")  && !weight.getText().toString().equals("") && !birthDay.getText().toString().equals("")) {
+            Child child = new Child(firstName.getText().toString(), lastName.getText().toString(), fatherName.getText().toString(), birthDay.getText().toString(), age.getText().toString(), weight.getText().toString(), smes.getSelectedItem().toString());
+            firebaseDatabase.push().setValue(child);
+            startActivity(new Intent(AddChildActivity.this, MainActivity.class));
+        }
+        else{
+            Toast.makeText(getApplicationContext(), "Заполните все поля", Toast.LENGTH_SHORT).show();
+//
+        }
     }
 }
--- app/src/main/java/com/example/myapplication/Child.java
+++ app/src/main/java/com/example/myapplication/Child.java
@@ -1,16 +1,31 @@
 package com.example.myapplication;
 
 public class Child {
-    public String  firstName, lastName, fatherName, birthDay;
+    public String  firstName, lastName, fatherName, birthDay, smes, age, weight;
 
+
+    public Child(String firstName, String lastName, String fatherName, String birthDay, String age, String weight, String smes) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.fatherName = fatherName;
+        this.birthDay = birthDay;
+        this.age = age;
+        this.weight = weight;
+        this.smes = smes;
+    }
     public Child(String firstName, String lastName, String fatherName, String birthDay) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.fatherName = fatherName;
         this.birthDay = birthDay;
+
     }
+
     public Child(){}
 
+    public String getSmes() {
+        return smes;
+    }
 
     public String getFirstName() {
         return firstName;
@@ -28,5 +43,11 @@ public class Child {
         return birthDay;
     }
 
+    public String getAge() {
+        return age;
+    }
 
+    public String getWeight() {
+        return weight;
+    }
 }
